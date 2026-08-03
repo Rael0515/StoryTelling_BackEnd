a73b86b91473202849d707dd46da4048bd39b1ac
Rael0515 <sangjinmusou@gmail.com>
2026-08-03 20:56:55 +0900

    스토리생성후출력내용추가

--- MakeStory.py
+++ MakeStory.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 class StroyRequest(BaseModel):
     keywords: list[str]
 
+
 @app.post("/api/generate")
 def generate_story(request: StroyRequest):
     return {"story": "'{','.join(request.keywords)}' 키워드로 만든 짧은 스토리 입니다."}
