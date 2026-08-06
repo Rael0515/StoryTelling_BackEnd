f2b276c5e6e233b36107d938679735083727d13a
Rael0515 <perey0515@naver.com>
2026-08-06 14:45:43 +0900

    컨피그와 스토리 py 파일 수정

--- core/config.py
+++ core/config.py
@@ -1,10 +1,12 @@
 from fastapi.middleware.cors import CORSMiddleware
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # 나중에 Vercel 도메인 추가
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+def setup_cors(app):
+    app.add_middleware( #미들웨어는 모든 app가 거치는 함수
+        ##
+        CORSMiddleware,
+        allow_origins=["http://localhost:5173"],  # 나중에 Vercel 도메인 추가
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 ## 임시 파일
--- routers/story.py
+++ routers/story.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
+from fastapi import APIRouter
+router = APIRouter()
 
-class StroyRequest(BaseModel):
+
+class StoryRequest(BaseModel):
     keywords: list[str]
 
-
-@app.post("/api/generate")
-def generate_story(request: StroyRequest):
-    return {"story": "'{','.join(request.keywords)}' 키워드로 만든 짧은 스토리 입니다."}
+@router.post("/api/generate")
+def generate_story(request: StoryRequest):
+    return {"story": f"'{','.join(request.keywords)}' 키워드로 만든 짧은 스토리 입니다."}
