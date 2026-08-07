4d5db5e7f46519addfd534269e9f26fd4944c458
Rael0515 <sangjinmusou@gmail.com>
2026-08-07 22:27:30 +0900

    데모수정

--- Demo.py
+++ Demo.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],  # 나중에 Vercel 도메인 추가
+    
     allow_methods=["*"],
     allow_headers=["*"],
 )
