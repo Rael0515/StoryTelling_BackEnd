a072ba9f8657813d49acb896fa533eb8e22d7112
Rael0515 <sangjinmusou@gmail.com>
2026-08-09 12:25:19 +0900

    Clean up CORS middleware configuration
    Removed unnecessary comment from CORS middleware setup.

--- core/config.py
+++ core/config.py
@@ -2,7 +2,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 def setup_cors(app):
     app.add_middleware( #미들웨어는 모든 app가 거치는 함수
-        ##
         CORSMiddleware,
         allow_origins=["http://localhost:5173"],  # 나중에 Vercel 도메인 추가
         allow_methods=["*"],
