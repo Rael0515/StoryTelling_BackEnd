a5cafb7628d19b60f77a37ecb8daa881e250c392
Rael0515 <sangjinmusou@gmail.com>
2026-08-19 23:29:57 +0900

    Add health check endpoint to main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from core.config import setup_cors
 from routers import story
+
 app = FastAPI()
 setup_cors(app)
 
 app.include_router(story.router)
+
 @app.get("/")
 def health_check():
     return {"status": "ok"}
